f43938510e0a496f1545648329f68d96fa5c6baf
uaxvit <130971780+uaxvit@users.noreply.github.com>
2025-03-15 22:20:58 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,6 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+nameInput.addEventListener('input', event => {
+  const trimmedValue = event.target.value.trim();
+  nameOutput.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
+});
